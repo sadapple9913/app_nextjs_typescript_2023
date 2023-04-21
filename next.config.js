6d834e7d18993f912afbbd181badfd54d9a91a31
sadapple <127265898+sadapple9913@users.noreply.github.com>
2023-04-21 14:14:48 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== 'production'
+const name = 'app_nextjs_typescript_2023'
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: !debug ? `/${name}/`:'',
+  basePath : '/app_extjs_typescript_2023',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
